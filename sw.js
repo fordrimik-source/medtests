56010b7d18bdccc8fe66a3b8547afcc4173a248a
fordrimik-source <fordrimik@gmail.com>
2026-06-19 16:11:48 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'medtests-v1';
+const ASSETS = [
+  './',
+  './patient.html',
+  './doctor.html',
+  './manifest.webmanifest'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+    if (resp.ok) { const clone = resp.clone(); caches.open(CACHE).then(c => c.put(e.request, clone)); }
+    return resp;
+  })));
+});
